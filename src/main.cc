60e1221d4d401b9626917e636c36330ab4e476d9
Travis <travis_chamness1@my.vcccd.edu>
2018-10-01 11:15:38 -0700

    10/1 REDO Using Insertion sort to sort Three Integers by reference.

---
+++ src/main.cc
@@ -0,0 +1,24 @@
+/**
+* CS V13 Data Structures and Algorithms
+* Assignment: EX03 - Three Integer Sort
+*
+* Statement of code ownership: I hereby state that I have written all of this
+* code and I have not copied this code from any other person or source.
+*
+* @author travis_chamness1@my.vcccd.edu
+*/
+#include<iostream>
+#include"threeintegersort.h"
+using edu::vcccd::vc::csv13::sort;
+using std::cout;
+using std::endl;
+
+void main(int argc, char *argv) {
+    int64_t a = 15;
+    int64_t b = 1;
+    int64_t c = 66;
+
+    sort(a, b, c);
+    cout << "a: " << a << "b: " << b << "c: " << c << endl;
+
+}
