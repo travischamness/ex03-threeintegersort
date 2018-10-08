9e2053a92ee14d1e1f93c763863e70e6ea84dfab
Travis <travis_chamness1@my.vcccd.edu>
2018-10-08 06:25:08 -0700

    10/8 integer reconfig in sort.cc

---
+++ include/threeintegersort.h
@@ -0,0 +1,13 @@
+#ifndef EX03_THREEINTEGERSORT_THREEINTEGERSORT_H
+#define EX03_THREEINTEGERSORT_THREEINTEGERSORT_H
+namespace edu {
+    namespace vcccd {
+        namespace vc {
+            namespace csv13 {
+                int sort(int64_t &a, int64_t &b, int64_t &c);
+            }
+        }
+    }
+}
+#endif //EX03_THREEINTEGERSORT_THREEINTEGERSORT_H
+
---
+++ src/sorter.cc
@@ -0,0 +1,22 @@
+#include"threeintegersort.h"
+namespace edu {
+    namespace vcccd {
+        namespace vc {
+            namespace csv13 {
+
+                void sort(int64_t a, int b, int c) {//no return
+                    int array[] = (&a, &b, &c);
+                    for (int i = 0; i <= 2; i++) {
+                        int j = i;
+                        if (array[j] >= array[j - 1]) {
+                            int hold = array[j];
+                            array[j] = array[j - 1];
+                            array[j - 1] = hold;
+                            --j;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
